d9f6ef0a7200c3ddcb6acfa488b4ffdb7e6ac83b
Kevin London <kevinlondon@gmail.com>
2017-09-26 12:52:51 -0700

    End of 1-getting-started

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const networkInterface = createNetworkInterface({
+  uri: '__SIMPLE_API_ENDPOINT__',
+})
+
+const client = new ApolloClient({
+  networkInterface
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+)
+registerServiceWorker()
