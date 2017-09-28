11658470eb49fa90f0b28b6acb8528d09e8b6e05
Kevin London <kevinlondon@gmail.com>
2017-09-27 22:53:37 -0700

    End of chapter 6

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+import { GC_AUTH_TOKEN } from './constants'
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj8202z9w00500100ki2eclj8',
 })
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
 
 const client = new ApolloClient({
   networkInterface
