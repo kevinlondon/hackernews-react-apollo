610cab8e50d491e33ae04d9680bed4394998d8af
Kevin London <kevinlondon@gmail.com>
2017-09-27 22:48:24 -0700

    Adding in mutation for creation / login

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+import Login from './Login'
 import Header from './Header'
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
@@ -12,6 +13,7 @@ class App extends Component {
           <Header />
           <div class='ph3 pv1 background-gray'>
             <Switch>
+              <Route exact path='/login' component={Login}/>
               <Route exact path='/' component={LinkList}/>
               <Route exact path='/create' component={CreateLink}/>
             </Switch>
